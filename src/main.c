0310f33d6a1bb532bb6776eaf17fa4bf3bd5a940
Arnaud Tiret <arnaud.tiret@gmail.com>
2012-04-11 19:38:11 +0200

    VS fonctionnel

--- src/main.c
+++ src/main.c
@@ -18,13 +18,11 @@ char initialization()
 		printf("Play vs computer (1) or human (2)?\n");
 		scanf("%d",&mode);
 	}while(mode != 1 && mode != 2);
-	
+        printf("Do you want to begin? (y: yes) (n: no)\n");
 	do
         {
-                printf("Do you want to begin? (n: no) (y: yes)\n");
                 scanf("%c", &begin);
         }while(begin != 'y' && begin != 'n');
-
         if(begin == 'y')
                 printf("You begin\n");
         else
@@ -34,38 +32,74 @@ char initialization()
 
 void computerTurn()
 {
-
+	printf("Computer's turn \n");
+	//TODO
 }
 
 void playerTurn(int playerNb)
 {
 	int nbHeap, nbSticks;
-	
-	printf("Player %d's turn\n", playerNb);
 	do
 	{
-		printf("Which heap do you want to remove sticks from?\n");
-		scanf("%d", &nbHeap);
-	}while(nbHeap < 1 || nbHeap > 3);
+		printf("Player %d's turn\n", playerNb);
+		do
+		{
+			printf("Which heap do you want to remove sticks from?\n");
+			scanf("%d", &nbHeap);
+		}while(nbHeap < 1 || nbHeap > 3);
 	
-	do
-	{
-		printf("How much do you want to remove sticks\n");
-		scanf("%d", &nbSticks);
-	}while(nbSticks < 1 || nbSticks > 3);
-	
+		do
+		{
+			printf("How much do you want to remove sticks\n");
+			scanf("%d", &nbSticks);
+		}while(nbSticks < 1 || nbSticks > 3);
+		
+	}while(isValidMove(nbHeap, nbSticks) == 0);
+		
 	if (nbHeap == 1)
 		heapA = heapA >> nbSticks;
 	if (nbHeap == 2)	
 		heapB = heapB >> nbSticks;
 	if (nbHeap == 3)
-                heapC = heapC >> nbSticks;
-
+           	heapC = heapC >> nbSticks;
 }
 
-int validMove(heapNb, nbSticks)
+int isValidMove(heapNb, nbSticks)
 {
-	//TODO
+	int valid = 1;
+	if(heapNb == 1)
+	{
+		if(nbSticks == 1 && heapA == 0x00)
+			  valid = 0;
+		if(nbSticks == 2 && heapA <= 0x01)
+			  valid = 0;
+		if(nbSticks == 3 && heapA <= 0x03)
+			  valid = 0;
+	}
+	if(heapNb == 2)
+	{
+		if(nbSticks == 1 && heapB == 0x00)
+			  valid = 0;
+		if(nbSticks == 2 && heapB <= 0x01)
+			  valid = 0;
+		if(nbSticks == 3 && heapB <= 0x03)
+			  valid = 0;
+	}
+	if(heapNb == 3)
+	{
+		if(nbSticks == 1 && heapC == 0x00)
+			  valid = 0;
+		if(nbSticks == 2 && heapC <= 0x01)
+			  valid = 0;
+		if(nbSticks == 3 && heapC <= 0x03)
+			  valid = 0;
+	}
+	
+	if(valid == 0)
+	{
+		  printf("Invalid move !\n");
+		  return 0;
+	}
 	return 1;
 }
 
@@ -117,18 +151,24 @@ int endGame()
 		return 1;
 }
 
-void conclusion()
+void conclusion(int looser)
 {
 	if(mode == 1)
 	{
-		printf("Display winner in Computer mode\n");
+		if(looser == 3)
+			printf("You lost against the computer!");
+		else
+			printf("You won");
 	}
 	else
 	{
-		printf("Display winner in  mode\n");
+		if(looser == 1)
+			printf("Player 1 is the Winner!!\n");
+		else
+			printf("Player 2 is the Winner!!\n");
 	}
 
-
+	//TODO
 	//printf("Do you want to display full log of the game?\n");
 	
 }
@@ -136,7 +176,8 @@ void conclusion()
 void main(void)
 {
 	char begin;
-	int toPlay, win;	
+	int toPlay, win, looser;	
+	int computerPlayed;
 
 	begin = initialization();
 	if(begin == 'y')
@@ -145,24 +186,41 @@ void main(void)
 		toPlay = 2;
 	do
 	{
+		displayGame();
 		if(toPlay == 1)
 		{
 			if(mode == 1 && begin != 'y')
-                                computerTurn();
-                        else
-                                playerTurn(toPlay);
-				toPlay == 2;
+			{	
+				computerTurn();
+				computerPlayed = 1;
+			}
+			else
+			{
+				playerTurn(toPlay);
+				computerPlayed = 0;
+			}
+			toPlay = 2;
 		}
 		else if(toPlay == 2)
 		{
 			if(mode == 1 && begin == 'y')
+			{
 				computerTurn();
+				computerPlayed = 1;
+			}
 			else
+			{
 				playerTurn(toPlay);
-			toPlay == 1;
+				computerPlayed = 0;
+			}
+			toPlay = 1;
 		}
 		displayGame();	
 	}
 	while(endGame() == 1);
-	conclusion();
+	if(mode == 1 && computerPlayed == 1)
+		looser = 3;
+		
+	looser = toPlay;
+	conclusion(looser);
 }
