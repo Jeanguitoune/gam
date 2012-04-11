dbd72db0e3e4b5d51a30c8738bac10ee6ac393c5
Arnaud Tiret <arnaud.tiret@gmail.com>
2012-04-11 02:05:09 +0200

    No biggie

--- main.c
+++ main.c
@@ -10,8 +10,11 @@ int decrementHeap(int heap, int nb)
 
 void displayGame(int heapA, int heapB, int heapC)
 {
-	//printf();
-	
+	printf("=================================\n");
+	printf("heapA: %d\n",heapA);
+	printf("heapB: %d\n",heapB);
+	printf("heapC: %d\n",heapC);
+	printf("=================================\n");
 }	
 
 void displayLog()
@@ -29,12 +32,9 @@ int endGame(int heapA, int heapB, int heapC)
 void main(void)
 {
 	unsigned int heapA, heapB, heapC;
-	int begin;
+	char begin;
+	int nbSticks, nbHeap;
 	int win;	
-
-	//========================
-	// Initialization
-	//========================
 
 	heapA = 0x3f;// 6 sticks heap
 	heapB = 0x1f;// 5 sticks heap
@@ -42,37 +42,41 @@ void main(void)
 	
 	do
 	{ 
-		printf("Do you want to begin? (0: No) (1: Yes)\n");
-		scanf("%d", &begin);
-	}while(begin != 0 && begin != 1);
+		printf("Do you want to begin? (n: no) (y: yes)\n");
+		scanf("%c", &begin);
+	}while(begin != 'y' && begin != 'n');
 	
-	if(begin == 1)
+	if(begin == 'y')
 		printf("You begin\n");
 	else
 		printf("The AI begin\n");
 
+	displayGame(heapA, heapB, heapC);	
 	do
 	{
-	printf("Which heap do you want to remove sticks from?");
-	scanf("%d", &nbHeap);
-	}while();
+		printf("Which heap do you want to remove sticks from?\n");
+		scanf("%d", &nbHeap);
+	}while(nbHeap < 1 || nbHeap > 3);
 	
 	do
 	{
-	printf("How much do you want to remove sticks");
-	scanf("%d", &nbSticks);
-	}while(nbSticks < 1 && nbSticks > 3);
+		printf("How much do you want to remove sticks\n");
+		scanf("%d", &nbSticks);
+	}while(nbSticks < 1 || nbSticks > 3);
 	
 	if (nbHeap == 1)
-		heapA = decrementHeap(nbHeap, nbSticks);
-	
+		heapA = decrementHeap(heapA, nbSticks);
+	if (nbHeap == 2)	
+		heapB = decrementHeap(heapB, nbSticks);
+	if (nbHeap == 3)
+                heapC = decrementHeap(heapC, nbSticks);
 
+	displayGame(heapA, heapB, heapC);	
 	/*
 	do
 	{
 		printf();
 		scanf();
-	
 
 	}while( choosen_heap > 3 || choosen_heap < 1 );
 	*/
